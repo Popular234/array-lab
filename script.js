b3e04e6d73b3ab872f5f0f012216e1f7af9d7ac5
Popular234 <cainjose24@g.crookcountyschools.org>
2023-01-31 13:39:27 -0800

    yeap

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// console.log("hello world")
+
+const drank =["Soda ", "Water ", "Appy Jewce ", "Spicy Water"]
+
+drank.push('Tea')
+
+console.log(drank)
+
+const fewd =["pankace ", "cerbeal ", "pupperfish ", "bucket of shrimp"]
+
+fewd.pop()
+
+for(let i=0; i<fewd.length; i++){
+    (fewd[i]);
+    document.write(fewd[i])
+}
